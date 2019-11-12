b990b203c81a60892e6970650c390d22872829c4
Doug Watson <dougwatson@outlook.com>
2019-11-11 18:42:26 -0900

    updated how moviegoers are handled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+  #debugger
   before_action :set_current_user
   protected
   def set_current_user
     @current_user ||= Moviegoer.where(:id => session[:user_id])
-    redirect_to login_path and return unless @current_user
+    redirect_to login_path and return unless session[:user_id]
+    #@current_user
   end
 end
