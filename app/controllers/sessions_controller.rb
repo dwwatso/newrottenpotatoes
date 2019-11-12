83d4a454f765ede0f609eed376898730b6bdbdac
Doug Watson <dougwatson@outlook.com>
2019-11-11 15:09:54 -0900

    skip action callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_filter :set_current_user
+  skip_action_callback :set_current_user
   def create
     auth=request.env["omniauth.auth"]
     user=Moviegoer.where(:provider => auth["provider"], 
