89a7d6f39324a5ac804bb9c91eacce119148e8de
Doug Watson <dougwatson@outlook.com>
2019-11-12 20:07:40 -0900

    bug fixes to app controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
   protected
   def set_current_user
-    @current_user ||= Moviegoer.find(session[:user_id]) if session[:user_id]
+    #debugger
+    @current_user ||= Moviegoer.find_by(id: session[:user_id]) if session[:user_id]
     redirect_to login_path and return unless @current_user
   end
 end
